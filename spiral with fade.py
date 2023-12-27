1a3b447521b701e3716cd87bb4e6186e70a7a65c
Tom Jebbo <ThomasJebbo@gmail.com>
2023-12-27 16:49:16 +0000

    new videos soon!

---
+++ spiral with fade.py
@@ -0,0 +1,250 @@
+import numpy as np
+import pygame
+import math
+import time
+import random
+from math import floor
+from ctypes import c_int64
+import sys
+import numba
+
+GRADIENTS2 = np.array([
+    5, 2, 2, 5,
+    -5, 2, -2, 5,
+    5, -2, 2, -5,
+    -5, -2, -2, -5,
+], dtype=np.int64)
+
+GRADIENTS3 = np.array([
+    -11, 4, 4, -4, 11, 4, -4, 4, 11,
+    11, 4, 4, 4, 11, 4, 4, 4, 11,
+    -11, -4, 4, -4, -11, 4, -4, -4, 11,
+    11, -4, 4, 4, -11, 4, 4, -4, 11,
+    -11, 4, -4, -4, 11, -4, -4, 4, -11,
+    11, 4, -4, 4, 11, -4, 4, 4, -11,
+    -11, -4, -4, -4, -11, -4, -4, -4, -11,
+    11, -4, -4, 4, -11, -4, 4, -4, -11,
+], dtype=np.int64)
+
+GRADIENTS4 = np.array([
+    3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3,
+    -3, 1, 1, 1, -1, 3, 1, 1, -1, 1, 3, 1, -1, 1, 1, 3,
+    3, -1, 1, 1, 1, -3, 1, 1, 1, -1, 3, 1, 1, -1, 1, 3,
+    -3, -1, 1, 1, -1, -3, 1, 1, -1, -1, 3, 1, -1, -1, 1, 3,
+    3, 1, -1, 1, 1, 3, -1, 1, 1, 1, -3, 1, 1, 1, -1, 3,
+    -3, 1, -1, 1, -1, 3, -1, 1, -1, 1, -3, 1, -1, 1, -1, 3,
+    3, -1, -1, 1, 1, -3, -1, 1, 1, -1, -3, 1, 1, -1, -1, 3,
+    -3, -1, -1, 1, -1, -3, -1, 1, -1, -1, -3, 1, -1, -1, -1, 3,
+    3, 1, 1, -1, 1, 3, 1, -1, 1, 1, 3, -1, 1, 1, 1, -3,
+    -3, 1, 1, -1, -1, 3, 1, -1, -1, 1, 3, -1, -1, 1, 1, -3,
+    3, -1, 1, -1, 1, -3, 1, -1, 1, -1, 3, -1, 1, -1, 1, -3,
+    -3, -1, 1, -1, -1, -3, 1, -1, -1, -1, 3, -1, -1, -1, 1, -3,
+    3, 1, -1, -1, 1, 3, -1, -1, 1, 1, -3, -1, 1, 1, -1, -3,
+    -3, 1, -1, -1, -1, 3, -1, -1, -1, 1, -3, -1, -1, 1, -1, -3,
+    3, -1, -1, -1, 1, -3, -1, -1, 1, -1, -3, -1, 1, -1, -1, -3,
+    -3, -1, -1, -1, -1, -3, -1, -1, -1, -1, -3, -1, -1, -1, -1, -3,
+], dtype=np.int64)
+
+STRETCH_CONSTANT2 = -0.211324865405187
+SQUISH_CONSTANT2 = 0.366025403784439
+STRETCH_CONSTANT3 = -1.0 / 6
+SQUISH_CONSTANT3 = 1.0 / 3
+STRETCH_CONSTANT4 = -0.138196601125011
+SQUISH_CONSTANT4 = 0.309016994374947
+
+NORM_CONSTANT2 = 47
+NORM_CONSTANT3 = 103
+NORM_CONSTANT4 = 30
+
+@numba.njit(cache=True)
+def extrapolate2(perm, xsb, ysb, dx, dy):
+    index = perm[(perm[xsb & 0xFF] + ysb) & 0xFF] & 0x0E
+    g1, g2 = GRADIENTS2[index : index + 2]
+    return g1 * dx + g2 * dy
+
+@numba.njit(cache=True)
+def generatekey(x, y, perm):
+    stretch_offset = (x + y) * STRETCH_CONSTANT2
+
+    xs = x + stretch_offset
+    ys = y + stretch_offset
+
+    xsb = floor(xs)
+    ysb = floor(ys)
+
+    squish_offset = (xsb + ysb) * SQUISH_CONSTANT2
+    xb = xsb + squish_offset
+    yb = ysb + squish_offset
+
+    xins = xs - xsb
+    yins = ys - ysb
+
+    in_sum = xins + yins
+
+    dx0 = x - xb
+    dy0 = y - yb
+
+    value = 0
+
+    dx1 = dx0 - 1 - SQUISH_CONSTANT2
+    dy1 = dy0 - 0 - SQUISH_CONSTANT2
+    attn1 = 2 - dx1 * dx1 - dy1 * dy1
+    if attn1 > 0:
+        attn1 *= attn1
+        value += attn1 * attn1 * extrapolate2(perm, xsb + 1, ysb + 0, dx1, dy1)
+
+    dx2 = dx0 - 0 - SQUISH_CONSTANT2
+    dy2 = dy0 - 1 - SQUISH_CONSTANT2
+    attn2 = 2 - dx2 * dx2 - dy2 * dy2
+    if attn2 > 0:
+        attn2 *= attn2
+        value += attn2 * attn2 * extrapolate2(perm, xsb + 0, ysb + 1, dx2, dy2)
+
+    if in_sum <= 1:
+        zins = 1 - in_sum
+        if zins > xins or zins > yins:
+            if xins > yins:
+                xsv_ext = xsb + 1
+                ysv_ext = ysb - 1
+                dx_ext = dx0 - 1
+                dy_ext = dy0 + 1
+            else:
+                xsv_ext = xsb - 1
+                ysv_ext = ysb + 1
+                dx_ext = dx0 + 1
+                dy_ext = dy0 - 1
+        else:
+            xsv_ext = xsb + 1
+            ysv_ext = ysb + 1
+            dx_ext = dx0 - 1 - 2 * SQUISH_CONSTANT2
+            dy_ext = dy0 - 1 - 2 * SQUISH_CONSTANT2
+    else:
+        zins = 2 - in_sum
+        if zins < xins or zins < yins:
+            if xins > yins:
+                xsv_ext = xsb + 2
+                ysv_ext = ysb + 0
+                dx_ext = dx0 - 2 - 2 * SQUISH_CONSTANT2
+                dy_ext = dy0 + 0 - 2 * SQUISH_CONSTANT2
+            else:
+                xsv_ext = xsb + 0
+                ysv_ext = ysb + 2
+                dx_ext = dx0 + 0 - 2 * SQUISH_CONSTANT2
+                dy_ext = dy0 - 2 - 2 * SQUISH_CONSTANT2
+        else:
+            dx_ext = dx0
+            dy_ext = dy0
+            xsv_ext = xsb
+            ysv_ext = ysb
+        xsb += 1
+        ysb += 1
+        dx0 = dx0 - 1 - 2 * SQUISH_CONSTANT2
+        dy0 = dy0 - 1 - 2 * SQUISH_CONSTANT2
+
+    attn0 = 2 - dx0 * dx0 - dy0 * dy0
+    if attn0 > 0:
+        attn0 *= attn0
+        value += attn0 * attn0 * extrapolate2(perm, xsb, ysb, dx0, dy0)
+
+    attn_ext = 2 - dx_ext * dx_ext - dy_ext * dy_ext
+    if attn_ext > 0:
+        attn_ext *= attn_ext
+        value += attn_ext * attn_ext * extrapolate2(perm, xsv_ext, ysv_ext, dx_ext, dy_ext)
+
+    return value / NORM_CONSTANT2
+
+def overflow(x):
+    return c_int64(x).value
+
+def getseed(seed):
+    perm = np.zeros(256, dtype=np.int64)
+    perm_grad_index3 = np.zeros(256, dtype=np.int64)
+    source = np.arange(256)
+
+    seed = overflow(seed * 6364136223846793005 + 1442695040888963407)
+    seed = overflow(seed * 6364136223846793005 + 1442695040888963407)
+    seed = overflow(seed * 6364136223846793005 + 1442695040888963407)
+    for i in range(255, -1, -1):
+        seed = overflow(seed * 6364136223846793005 + 1442695040888963407)
+        r = int((seed + 31) % (i + 1))
+        if r < 0:
+            r += i + 1
+        perm[i] = source[r]
+
+        perm_grad_index3[i] = int((perm[i] % (len(GRADIENTS3) / 3)) * 3)
+        source[r] = source[i]
+
+    return perm
+
+pygame.init()
+
+display = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN, vsync=1)
+
+clock = pygame.time.Clock()
+
+start = time.perf_counter()
+
+def center_to_pygame(x, y, x_size=1920, y_size=1080):
+    new_x = x_size*x
+    new_y = y_size*y
+    return new_x, new_y
+
+radar = (1920/2, 1080/2)
+now_time = 0
+m = 0
+seed = getseed(random.randint(0, 10000000))
+seed2 = getseed(random.randint(0, 10000000))
+z = 0
+w = 0
+start = time.perf_counter()
+window_size = (1920, 1080)
+f = 0
+while True:
+    window_x, window_y = display.get_size()
+
+    Surface = pygame.Surface(window_size).convert()
+    Surface.fill((0, 0, 0))
+
+    Surface.set_colorkey((0, 0, 0))
+    Surface2 = pygame.Surface(window_size)
+
+    Surface2.fill((0, 0, 0))
+
+    radar_len = int((1+generatekey(-z/500, -w/500, seed2))*(1080/4))
+
+    x = radar[0] + math.cos(now_time) * radar_len
+    y = radar[1] + math.sin(now_time) * radar_len
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                quit()
+
+            if event.key == pygame.K_SPACE:
+                display.fill([0, 0, 0])
+
+    r = int((1+generatekey(z/500, w/500, seed))*(255/2))
+    g = int((1+generatekey(z/500, -w/500, seed))*(255/2))
+    b = int((1+generatekey(-z/500, w/500, seed))*(255/2))
+
+    pygame.draw.line(Surface, [r, g, b], radar, (x, y), width=10)
+    pygame.draw.line(Surface, [r, g, b], radar, (1920-x, 1080-y), width=10)
+
+    Surface2.set_alpha(1) # 10
+
+    if f > int((1+generatekey(-z/1000, w/1000, seed))*(10/2)):
+        display.blit(Surface2, (0, 0))
+        f = 0
+
+    display.blit(Surface, (0, 0))
+
+    pygame.display.flip()
+    clock.tick(500)
+    now_time = time.perf_counter()-start
+    z += 1
+    w -= 1
+    f += 1
---
+++ spiral without fade.py
@@ -0,0 +1,276 @@
+import pygame, math, time, threading, pyaudio, os
+import numpy as np
+
+def get_audio_data():
+    global volume, v
+
+    # Audio variables
+    CHUNK = 2048#4096
+    RATE = 44100
+    power = 12
+
+    # Opens audio stream
+    p = pyaudio.PyAudio()
+    stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,frames_per_buffer=CHUNK)
+
+    mean = [0, 0]
+    volume = 0
+    v = 0
+    vmax = 0
+
+    while True:
+        # Reads the data
+        data = np.frombuffer(stream.read(CHUNK),dtype=np.int16)
+
+        # Calculates the peak of the frequency
+        peak = np.average(np.abs(data))*2
+
+        # Shows the bars for amplitude
+        if peak > mean[0]:
+            volume += 1
+        else:
+            volume -= volume/1.5
+
+        mean[0] = (mean[0]*mean[1] + peak)/(mean[1]+1)
+        mean[1] += 1
+
+        if peak > vmax:
+            vmax = peak
+        v = (peak/vmax)*500
+
+    stream.stop_stream()
+    stream.close()
+
+import pygame
+import math
+import time
+import random
+import numpy as np
+from math import floor
+from ctypes import c_int64
+import random
+import sys
+import numba
+
+GRADIENTS2 = np.array([
+    5, 2, 2, 5,
+    -5, 2, -2, 5,
+    5, -2, 2, -5,
+    -5, -2, -2, -5,
+], dtype=np.int64)
+
+GRADIENTS3 = np.array([
+    -11, 4, 4, -4, 11, 4, -4, 4, 11,
+    11, 4, 4, 4, 11, 4, 4, 4, 11,
+    -11, -4, 4, -4, -11, 4, -4, -4, 11,
+    11, -4, 4, 4, -11, 4, 4, -4, 11,
+    -11, 4, -4, -4, 11, -4, -4, 4, -11,
+    11, 4, -4, 4, 11, -4, 4, 4, -11,
+    -11, -4, -4, -4, -11, -4, -4, -4, -11,
+    11, -4, -4, 4, -11, -4, 4, -4, -11,
+], dtype=np.int64)
+
+GRADIENTS4 = np.array([
+    3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3,
+    -3, 1, 1, 1, -1, 3, 1, 1, -1, 1, 3, 1, -1, 1, 1, 3,
+    3, -1, 1, 1, 1, -3, 1, 1, 1, -1, 3, 1, 1, -1, 1, 3,
+    -3, -1, 1, 1, -1, -3, 1, 1, -1, -1, 3, 1, -1, -1, 1, 3,
+    3, 1, -1, 1, 1, 3, -1, 1, 1, 1, -3, 1, 1, 1, -1, 3,
+    -3, 1, -1, 1, -1, 3, -1, 1, -1, 1, -3, 1, -1, 1, -1, 3,
+    3, -1, -1, 1, 1, -3, -1, 1, 1, -1, -3, 1, 1, -1, -1, 3,
+    -3, -1, -1, 1, -1, -3, -1, 1, -1, -1, -3, 1, -1, -1, -1, 3,
+    3, 1, 1, -1, 1, 3, 1, -1, 1, 1, 3, -1, 1, 1, 1, -3,
+    -3, 1, 1, -1, -1, 3, 1, -1, -1, 1, 3, -1, -1, 1, 1, -3,
+    3, -1, 1, -1, 1, -3, 1, -1, 1, -1, 3, -1, 1, -1, 1, -3,
+    -3, -1, 1, -1, -1, -3, 1, -1, -1, -1, 3, -1, -1, -1, 1, -3,
+    3, 1, -1, -1, 1, 3, -1, -1, 1, 1, -3, -1, 1, 1, -1, -3,
+    -3, 1, -1, -1, -1, 3, -1, -1, -1, 1, -3, -1, -1, 1, -1, -3,
+    3, -1, -1, -1, 1, -3, -1, -1, 1, -1, -3, -1, 1, -1, -1, -3,
+    -3, -1, -1, -1, -1, -3, -1, -1, -1, -1, -3, -1, -1, -1, -1, -3,
+], dtype=np.int64)
+
+STRETCH_CONSTANT2 = -0.211324865405187
+SQUISH_CONSTANT2 = 0.366025403784439
+STRETCH_CONSTANT3 = -1.0 / 6
+SQUISH_CONSTANT3 = 1.0 / 3
+STRETCH_CONSTANT4 = -0.138196601125011
+SQUISH_CONSTANT4 = 0.309016994374947
+
+NORM_CONSTANT2 = 47
+NORM_CONSTANT3 = 103
+NORM_CONSTANT4 = 30
+
+@numba.njit(cache=True)
+def extrapolate2(perm, xsb, ysb, dx, dy):
+    index = perm[(perm[xsb & 0xFF] + ysb) & 0xFF] & 0x0E
+    g1, g2 = GRADIENTS2[index : index + 2]
+    return g1 * dx + g2 * dy
+
+@numba.njit(cache=True)
+def generatekey(x, y, perm):
+    stretch_offset = (x + y) * STRETCH_CONSTANT2
+
+    xs = x + stretch_offset
+    ys = y + stretch_offset
+
+    xsb = floor(xs)
+    ysb = floor(ys)
+
+    squish_offset = (xsb + ysb) * SQUISH_CONSTANT2
+    xb = xsb + squish_offset
+    yb = ysb + squish_offset
+
+    xins = xs - xsb
+    yins = ys - ysb
+
+    in_sum = xins + yins
+
+    dx0 = x - xb
+    dy0 = y - yb
+
+    value = 0
+
+    dx1 = dx0 - 1 - SQUISH_CONSTANT2
+    dy1 = dy0 - 0 - SQUISH_CONSTANT2
+    attn1 = 2 - dx1 * dx1 - dy1 * dy1
+    if attn1 > 0:
+        attn1 *= attn1
+        value += attn1 * attn1 * extrapolate2(perm, xsb + 1, ysb + 0, dx1, dy1)
+
+    dx2 = dx0 - 0 - SQUISH_CONSTANT2
+    dy2 = dy0 - 1 - SQUISH_CONSTANT2
+    attn2 = 2 - dx2 * dx2 - dy2 * dy2
+    if attn2 > 0:
+        attn2 *= attn2
+        value += attn2 * attn2 * extrapolate2(perm, xsb + 0, ysb + 1, dx2, dy2)
+
+    if in_sum <= 1:
+        zins = 1 - in_sum
+        if zins > xins or zins > yins:
+            if xins > yins:
+                xsv_ext = xsb + 1
+                ysv_ext = ysb - 1
+                dx_ext = dx0 - 1
+                dy_ext = dy0 + 1
+            else:
+                xsv_ext = xsb - 1
+                ysv_ext = ysb + 1
+                dx_ext = dx0 + 1
+                dy_ext = dy0 - 1
+        else:
+            xsv_ext = xsb + 1
+            ysv_ext = ysb + 1
+            dx_ext = dx0 - 1 - 2 * SQUISH_CONSTANT2
+            dy_ext = dy0 - 1 - 2 * SQUISH_CONSTANT2
+    else:
+        zins = 2 - in_sum
+        if zins < xins or zins < yins:
+            if xins > yins:
+                xsv_ext = xsb + 2
+                ysv_ext = ysb + 0
+                dx_ext = dx0 - 2 - 2 * SQUISH_CONSTANT2
+                dy_ext = dy0 + 0 - 2 * SQUISH_CONSTANT2
+            else:
+                xsv_ext = xsb + 0
+                ysv_ext = ysb + 2
+                dx_ext = dx0 + 0 - 2 * SQUISH_CONSTANT2
+                dy_ext = dy0 - 2 - 2 * SQUISH_CONSTANT2
+        else:
+            dx_ext = dx0
+            dy_ext = dy0
+            xsv_ext = xsb
+            ysv_ext = ysb
+        xsb += 1
+        ysb += 1
+        dx0 = dx0 - 1 - 2 * SQUISH_CONSTANT2
+        dy0 = dy0 - 1 - 2 * SQUISH_CONSTANT2
+
+    attn0 = 2 - dx0 * dx0 - dy0 * dy0
+    if attn0 > 0:
+        attn0 *= attn0
+        value += attn0 * attn0 * extrapolate2(perm, xsb, ysb, dx0, dy0)
+
+    attn_ext = 2 - dx_ext * dx_ext - dy_ext * dy_ext
+    if attn_ext > 0:
+        attn_ext *= attn_ext
+        value += attn_ext * attn_ext * extrapolate2(perm, xsv_ext, ysv_ext, dx_ext, dy_ext)
+
+    return value / NORM_CONSTANT2
+
+def overflow(x):
+    return c_int64(x).value
+
+def getseed(seed):
+    perm = np.zeros(256, dtype=np.int64)
+    perm_grad_index3 = np.zeros(256, dtype=np.int64)
+    source = np.arange(256)
+
+    seed = overflow(seed * 6364136223846793005 + 1442695040888963407)
+    seed = overflow(seed * 6364136223846793005 + 1442695040888963407)
+    seed = overflow(seed * 6364136223846793005 + 1442695040888963407)
+    for i in range(255, -1, -1):
+        seed = overflow(seed * 6364136223846793005 + 1442695040888963407)
+        r = int((seed + 31) % (i + 1))
+        if r < 0:
+            r += i + 1
+        perm[i] = source[r]
+
+        perm_grad_index3[i] = int((perm[i] % (len(GRADIENTS3) / 3)) * 3)
+        source[r] = source[i]
+
+    return perm
+
+pygame.init()
+
+display = pygame.display.set_mode((1000, 1000), vsync=1)
+
+clock = pygame.time.Clock()
+
+start = time.perf_counter()
+
+global volume, v, vmax
+
+volume = 0
+v = 0
+
+threading.Thread(target=get_audio_data).start()
+
+def center_to_pygame(x, y, x_size=1000, y_size=1000):
+    new_x = x_size*x
+    new_y = y_size*y
+    return new_x, new_y
+
+radar = (500, 500)
+radar_len = 500
+now_time = 0
+m = 0
+seed = getseed(random.randint(0, 10000000))
+seed2 = getseed(random.randint(0, 10000000))
+z = 0
+w = 0
+start = time.perf_counter()
+while True:
+    radar_len = int((1+generatekey(-z/500, -w/500, seed2))*250)
+
+    x = radar[0] + math.cos(now_time) * radar_len
+    y = radar[1] + math.sin(now_time) * radar_len
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                quit()
+
+    r = int((1+generatekey(z/500, w/500, seed))*(255/2))
+    g = int((1+generatekey(z/500, -w/500, seed))*(255/2))
+    b = int((1+generatekey(-z/500, w/500, seed))*(255/2))
+
+    pygame.draw.line(display, [r, g, b], radar, (x, y), width=10)
+    pygame.draw.line(display, [r, g, b], radar, (1000-x, 1000-y), width=10)
+
+    pygame.display.update()
+    clock.tick(120)
+    now_time = time.perf_counter()-start
+    z += 1
+    w -= 1
