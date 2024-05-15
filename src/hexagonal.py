472a5319129b178f5e208d25c9650a076a6d2e2a
Thomas Jebson <ThomasJebbo@gmail.com>
2024-05-15 11:29:28 +0100

    wowie, new effect and many new possibilities

---
+++ src/hexagonal.py
@@ -0,0 +1,87 @@
+
+import random
+import time
+import pmma
+import pygame
+import math
+
+canvas = pmma.Display()
+canvas.create(1280, 720)
+events = pmma.Events()
+
+registry = pmma.Registry
+
+color_perlin = pmma.Perlin(random.randint(0, 9999))
+rotation_perlin = pmma.Perlin(random.randint(0, 9999))
+
+SWITCH = False
+
+def draw_ngon(Surface, color, n, radius, position, rotation=0, width=0, wire_frame=False, cache=None):
+    if cache is not None:
+        return pygame.draw.polygon(Surface, color, cache, width=width), cache
+
+    pi2 = 2 * math.pi
+
+    if wire_frame:
+        for i in range(0, n):
+            pygame.draw.line(Surface, color, position, (math.cos(i / n * pi2) * radius + position[0], math.sin(i / n * pi2) * radius + position[1]))
+
+    points = [(math.cos(i / n * pi2 + rotation) * radius + position[0], math.sin(i / n * pi2 + rotation) * radius + position[1]) for i in range(0, n)]
+    if wire_frame:
+        width = 1
+
+    return pygame.draw.polygon(Surface, color, points, width=width), points
+
+class Hexagon:
+    def __init__(self, n):
+        self.n = n/10
+        self.size = n
+        self.iter = n
+        self.points = None
+
+    def render(self, now_time):
+        color = [
+            color_perlin.generate_2D_perlin_noise((now_time+self.n)/75, 0, [0, 255]),
+            color_perlin.generate_2D_perlin_noise(0, (now_time+self.n)/75, [0, 255]),
+            color_perlin.generate_2D_perlin_noise(-(now_time+self.n)/75, 0, [0, 255])
+        ]
+        if clear_cache:
+            self.points = None
+
+        _, self.points = draw_ngon(canvas.surface, color, 6, self.size, center, rotation=self.n/(2*math.pi))
+
+        if SWITCH:
+          self.n = rotation_perlin.generate_2D_perlin_noise(-(now_time+self.iter)/1000, 0, [0, 360]) # 500
+        else:
+          self.n += 0.5
+
+squares = []
+diag = int(math.sqrt(canvas.get_width()**2 + canvas.get_width()**2))
+for i in range(0, diag, 8):
+    squares.append(Hexagon(diag-i))
+
+clear_cache = False
+
+start = time.perf_counter()
+now_time = 0
+center = (canvas.get_width()/2, canvas.get_height()/2)
+while registry.running:
+    #k = time.perf_counter()
+
+    canvas.clear(pygame.transform.average_color(canvas.surface))
+
+    event = events.handle(canvas)
+    for e in event:
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_F11:
+                clear_cache = True
+                center = (canvas.get_width()/2, canvas.get_height()/2)
+
+    for square in squares:
+        square.render(now_time)
+
+    clear_cache = False
+
+    canvas.refresh(refresh_rate=60)
+    now_time = time.perf_counter() - start
+    #s = time.perf_counter()
